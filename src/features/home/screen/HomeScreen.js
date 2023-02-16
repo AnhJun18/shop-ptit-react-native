4a48710ba7d29222099647aafb69a4e6efab9354
ducmanhlai <ducmanhlai22@gmail.com>
2023-02-16 10:52:13 +0700

    add gradient background

--- src/features/home/screen/HomeScreen.js
+++ src/features/home/screen/HomeScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from 'react-native';
-
+import BackGround from "../../../common/components/Background";
 function HomeScreen(props) {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={{height:100}}>
+        <BackGround>
+        </BackGround>
+        <View styl><Text>hIhi</Text></View>
         </View>
-
+      
     )
 }
 export default HomeScreen;
