9370d42c998365485de3e4f4aae9471bd628f31a
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2023-05-18 15:41:25 +0700

    push notify

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import { store } from './src/reducer';
 import { Provider } from 'react-redux';
 import { AuthContextProvider } from './src/context/AuthProvider';
 import { navigationRef } from './src/navigations/RootNavigation';
-import {requestUserPermission,NotificationLister} from './src/common/utility/pushnotify-helper'
+import {requestUserPermission,NotificationLister} from './src/common/utility/pushNotificationHelper'
 import { useEffect } from 'react';
 function App() {
   useEffect(()=>{
--- index.js
+++ index.js
@@ -5,5 +5,26 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import PushNotification from "react-native-push-notification";
+import {Platform} from 'react-native'
+import { navigate } from './src/navigations/RootNavigation';
+PushNotification.configure({
+  onRegister: function (token) {
+    console.log("TOKEN:", token);
+  },
+  onNotification: function (notification) {
+    console.log("NOTIFICATION:", notification);
+    if(notification.data.productId)
+      navigate('ProductDetail', { itemID: notification.data.productId})
+  },
+
+  permissions: {
+    alert: true,
+    badge: true,
+    sound: true,
+  },
+  popInitialNotification: true,
+  requestPermissions: Platform.OS==="ios",
+});
 
 AppRegistry.registerComponent(appName, () => App);
